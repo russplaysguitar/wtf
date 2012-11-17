b9a92ab973e01b8a1a476dd3909d89b455ffe430
Russ <russplaysguitar@yahoo.com>
2012-11-17 02:34:55 -0800

    reverse order for questions#index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.order("created_at DESC")
 
     respond_to do |format|
       format.html # index.html.erb
