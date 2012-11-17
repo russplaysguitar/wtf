e3a8e8701c0782b3796892f67d4d37879b5eb421
Russ <russplaysguitar@yahoo.com>
2012-11-16 18:20:14 -0800

    update taggings migration

--- db/migrate/20121117015215_create_taggings.rb
+++ db/migrate/20121117015215_create_taggings.rb
@@ -1,7 +1,8 @@
 class CreateTaggings < ActiveRecord::Migration
   def change
     create_table :taggings do |t|
-
+			t.integer :question_id
+			t.integer :tag_id
       t.timestamps
     end
   end
