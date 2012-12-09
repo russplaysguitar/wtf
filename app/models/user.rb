9cc16aa1f8ad7ada4ab3315f23b8f0f62616db43
Russ <russplaysguitar@yahoo.com>
2012-12-09 12:49:17 -0800

    add some validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessible :username, :password, :password_confirmation
+  validates :username, :presence => true,
+            :uniqueness => {:case_sensitive => false},
+            :format => { :with => /\A\w+\Z/, :message => "is invalid"}
+  validates :password, :presence => true,
+            :length => { :in => 6..255 }  
   has_many :questions
   has_many :answers 
   has_many :comments
